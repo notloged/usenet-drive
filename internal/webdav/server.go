3097ad67d77b4873d92f6635747cee1c5a1aad92
javi11 <javicuba11@gmail.com>
2024-02-18 16:19:11 +0100

    fix: Update webdav server timeouts

--- internal/webdav/server.go
+++ internal/webdav/server.go
@@ -58,8 +58,8 @@ func (s *webdavServer) Start(ctx context.Context, port string) {
 	srv := &http.Server{
 		Addr: addr,
 		// Good practice to set timeouts to avoid Slowloris attacks.
-		WriteTimeout: time.Minute * 5,
-		IdleTimeout:  time.Second * 60,
+		IdleTimeout:  time.Minute * 5,
+		WriteTimeout: time.Minute * 30,
 		Handler:      mux,
 	}
 	s.log.InfoContext(ctx, fmt.Sprintf("WebDav server started at http://localhost:%v", port))
